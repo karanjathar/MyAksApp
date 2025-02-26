c6db66c580923b110f0503be747d2ea9ae1749ee
karanjathar <karanjathar@gmail.com>
2025-02-26 12:27:45 +0530

    info

--- obj/Debug/net8.0/MyAksApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyAksApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyAksApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19917a6ac82dbb911e4c8edfdc03310b6bb7f94e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyAksApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyAksApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
